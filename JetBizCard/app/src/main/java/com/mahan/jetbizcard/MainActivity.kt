71bf1de117b0af319cd91e8ed7b2499ecc1ca393
Mahan Ziyari <maziyar.mz.021@gmail.com>
2021-12-09 20:49:53 +0330

    Add ProfileImage

--- JetBizCard/app/src/main/java/com/mahan/jetbizcard/MainActivity.kt
+++ JetBizCard/app/src/main/java/com/mahan/jetbizcard/MainActivity.kt
@@ -4,18 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mahan.jetbizcard.ui.theme.JetBizCardTheme
@@ -51,8 +55,45 @@ fun BizCard() {
                 color = Color.LightGray
             )
         ) {
-
+            Column(
+                modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(12.dp))
+                ProfileImage()
+                Spacer(modifier = Modifier.height(2.dp))
+                Divider(
+                    color = MaterialTheme.colors.onSurface.copy(alpha = 0.3f),
+                    thickness = 3.dp
+                )
+            }
         }
+    }
+}
+
+@Composable
+private fun ProfileImage(
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        modifier = modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(
+            width = 0.5.dp,
+            color = Color.LightGray
+        ),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_image),
+            contentDescription = "Profile Image",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
     }
 }
 

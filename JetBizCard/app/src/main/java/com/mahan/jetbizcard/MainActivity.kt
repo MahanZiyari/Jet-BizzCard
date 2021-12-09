2aa7b10f5f3f5729a11f2e5a2a4e4c8e9ff92876
Mahan Ziyari <maziyar.mz.021@gmail.com>
2021-12-09 19:49:01 +0330

    Add Card Background

--- JetBizCard/app/src/main/java/com/mahan/jetbizcard/MainActivity.kt
+++ JetBizCard/app/src/main/java/com/mahan/jetbizcard/MainActivity.kt
@@ -3,11 +3,21 @@ package com.mahan.jetbizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mahan.jetbizcard.ui.theme.JetBizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +27,7 @@ class MainActivity : ComponentActivity() {
             JetBizCardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    BizCard()
                 }
             }
         }
@@ -25,14 +35,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BizCard() {
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(14.dp),
+            shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+            elevation = 10.dp,
+            border = BorderStroke(
+                width = 1.dp,
+                color = Color.LightGray
+            )
+        ) {
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetBizCardTheme {
-        Greeting("Android")
+        BizCard()
     }
 }
